7fd111f7b094a98bbdd2410a4808599aee8434d7
Cvetan <cvetanveljanovski@gmail.com>
2021-07-27 16:02:21 +0200

    Change test file

--- test_main.py
+++ test_main.py
@@ -2,3 +2,8 @@ from main import add (#the function to be tested)
 # here make the test to validate the function
 def test_add():
     assert add(1,2) == 3
+
+from main import hi
+    
+def test_hi():
+    assert hi == "Hi!"
